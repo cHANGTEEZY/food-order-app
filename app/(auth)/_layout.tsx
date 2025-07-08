d4c99be5bc9fe957dcc9dbc0c12861143cd2d3ab
cHANGTEEZY <jagatgurung30@gmail.com>
2025-07-08 22:56:53 +0545

    connected appwrite and user can create account

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,14 +1,44 @@
-import { Text, View } from "react-native";
+import {
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Dimensions,
+  Image,
+  ImageBackground,
+} from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { Slot } from "expo-router";
+import { images } from "@/constants";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
 
 const AuthLayout = () => {
   return (
-    <SafeAreaView className="items-center justify-center flex-1">
-      <Text>Welcome to Auth Layout</Text>
-      <Slot />
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+          <Image
+            source={images.logo}
+            className="self-center size-48 absolute -bottom-16 z-10"
+          />
+        </View>
+        <Slot />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,91 @@
-import { Button, Text, View } from "react-native";
-import React from "react";
-import { router } from "expo-router";
+import { Alert, Button, Text, View } from "react-native";
+import React, { useState } from "react";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { Link, router } from "expo-router";
+import { createUser } from "@/lib/appwrite";
 
 const SignUp = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
+
+  const submit = async () => {
+    const { name, email, password } = form;
+
+    if (!name || !email || !password) {
+      return Alert.alert(
+        "Error",
+        "Please enter valid name, email address and password",
+      );
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      await createUser({
+        name,
+        email,
+        password,
+      });
+
+      Alert.alert("Success", "User signed up successfully");
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <View>
-      <Button title="Sign up" onPress={() => router.push("/(auth)/sign-in")} />
+    <View className="gap-10 p-5 mt-5">
+      <CustomInput
+        placeholder="Enter your full name"
+        value={form.name}
+        onChangeText={(text) =>
+          setForm((prev) => ({
+            ...prev,
+            name: text,
+          }))
+        }
+        label="Full name"
+      />
+
+      <CustomInput
+        placeholder="Enter your email"
+        value={form.email}
+        onChangeText={(text) =>
+          setForm((prev) => ({
+            ...prev,
+            email: text,
+          }))
+        }
+        label="Email"
+        keyboardType="email-address"
+      />
+      <CustomInput
+        placeholder="Enter your password"
+        value={form.password}
+        onChangeText={(text) =>
+          setForm((prev) => ({
+            ...prev,
+            password: text,
+          }))
+        }
+        label="Password"
+        secureTextEntry={true}
+      />
+
+      <CustomButton title="Sign up" onPress={submit} />
+
+      <View className="flex justify-center mt-5 flex-row gap-2">
+        <Text className="base-regular text-gray-100">
+          Already have an account?{" "}
+        </Text>
+        <Link href="/sign-in" className="base-bold text-primary">
+          {isSubmitting ? "Submitting" : "Sign In"}
+        </Link>
+      </View>
     </View>
   );
 };
